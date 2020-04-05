d0206ff961601c0031e51d15a033570d3c836947
igors <spitkovskyigor@gmail.com>
2020-04-05 19:17:38 +0300

    initial commit

---
+++ src/main/java/net/springboot/learning/flightsmonitoring/FlightsMonitoringApplication.java
@@ -0,0 +1,13 @@
+package net.springboot.learning.flightsmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsMonitoringApplication.class, args);
+	}
+
+}
